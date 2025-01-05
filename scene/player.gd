f290f138fca49bb52a4abe2e693641ed2f619ecf
WLGfx <wlgfx@hotmail.com>
2025-01-05 09:37:17 +0000

    getting better with the ui

--- scene/player.gd
+++ scene/player.gd
@@ -3,27 +3,29 @@ extends CharacterBody3D
 @onready var camera: Camera3D = $mesh/camera
 @onready var player: CharacterBody3D = $"."
 
-@onready var mouse_speed: LineEdit = $"../Control/VBoxContainer/HBoxContainer/mouse speed"
+@onready var mouse_speed_le: LineEdit = $"Control/VBoxContainer/HBoxContainer/mouse speed"
+@onready var mouse_speed_sl: HSlider = $"Control/VBoxContainer/HBoxContainer2/mouse speed"
+
 
 # globals for mouse control
-var mouse_sensitivity = 0.002
+var mouse_sensitivity = 0.004
 var upDownMinAngle = -90.0
 var upDownMaxAngle = 90.0
 
 var mouse_active = false # handled by right mouse button
 
-var strafe_speed = 2 # units per second
-var strafe_multiplier = 0.5 # if boosted, strafe_speed is changed to 2 * strafe_multiplier
-var move_speed = 2 # units per second
-var move_multiplier = 0.5 # if boosted, move_speed is changed to 2 * move_multiplier
+var strafe_speed = 2 		# units per second
+var strafe_multiplier = 0.5 # increased on boost
+var move_speed = 2 			# units per second
+var move_multiplier = 0.5 	# increased on boost
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	mouse_speed.text = str(mouse_sensitivity)
+	mouse_speed_le.text = str(mouse_sensitivity)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	mouse_active = Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT)
 	
 	if Input.is_action_pressed("move boost"):
@@ -35,7 +37,7 @@ func _process(delta: float) -> void:
 	####
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var move_x : float = float(Input.get_action_strength("right")) - float(Input.get_action_strength("left"))
 	var move_z : float = float(Input.get_action_strength("backward")) - float(Input.get_action_strength("forward"))
 
@@ -59,5 +61,15 @@ func _input(event: InputEvent) -> void:
 
 
 func _on_mouse_speed_value_changed(value: float) -> void:
-	mouse_speed.text = str(value)
+	mouse_speed_le.text = str(value)
 	mouse_sensitivity = value
+
+
+func _on_mouse_speed_down_pressed() -> void:
+	if mouse_speed_sl.value > mouse_speed_sl.min_value:
+		mouse_speed_sl.value -= mouse_speed_sl.step
+
+
+func _on_mouse_speed_up_pressed() -> void:
+	if mouse_speed_sl.value < mouse_speed_sl.max_value:
+		mouse_speed_sl.value += mouse_speed_sl.step
